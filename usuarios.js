62cd40b034635f9d5572632b677ed14989a8d428
toko0715 <strongsitojheremy@gmail.com>
2026-03-23 11:34:28 -0500

    aa

--- usuarios.js
+++ usuarios.js
@@ -59,4 +59,8 @@ app.delete('/usuarios/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Servidor corriendo 🚀'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
